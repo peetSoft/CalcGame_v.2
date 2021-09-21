e51b9ccd621bbb27cf9bf4bd629c49400c1dde68
peterediger <peter.ediger@delta-v.us>
2021-09-21 08:45:41 +0200

    the title changed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from exercises import SimpleExercises, RandomExercises
 # Hello Pythionistas
 # This is a game!!!
+# This is a game again!!!
 # An ASCI-picture showing a player and a basket but no ball
 BASE_PIC = ''' 
               |                                  
