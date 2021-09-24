1af3858e4a4b96cdd51c51d406aeada72e1cc07f
peterediger <peter.ediger@delta-v.us>
2021-09-24 09:05:29 +0200

    new file organization

---
+++ sources/time_prototype.py
@@ -0,0 +1,36 @@
+import signal
+import time
+from contextlib import contextmanager
+
+
+class TimeoutException(Exception): pass
+
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
+
+def long_function_call():
+    i = 0
+    for i in range(10):
+        print(i)
+        time.sleep(1)
+        i += 1
+
+
+try:
+    with time_limit(8):
+        long_function_call()
+except TimeoutException as e:
+    print("Timed out!")
+else:
+    print("Exercise solved!")
